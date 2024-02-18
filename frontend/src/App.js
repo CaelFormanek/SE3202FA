036f904f7f9958e9926d1c13084d8d50b0fa1514
Cael Formanek <cformanek@chapman.edu>
2024-02-18 15:42:03 -0800

    more progress on landing page input boxes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,23 @@ function App() {
   return (
     <div className="App">
       <img className="logoimage" src={register} ></img>
-      <input className="emailinput" type="text" placeholder="Email"></input>
+
+      <div className="buttonsandinput">
+        <small className="emailheader">Email</small>
+        <input className="emailinput" type="text" placeholder=""></input>
+
+        <small className="createpasswordheader">Create Password</small>
+        <div>
+          <input className="createpasswordinput" type="text" placeholder=""></input>
+          <button className="togglecreatepassword">insert icon</button>
+        </div>
+        
+        <small className="confirmpasswordheader">Confirm Password</small>
+        <div>
+          <input className="confirmpasswordinput" type="text" placeholder=""></input>
+          <button className="toggleconfirmpassword">insert icon</button>
+        </div>
+      </div>
     </div>
   );
 }
