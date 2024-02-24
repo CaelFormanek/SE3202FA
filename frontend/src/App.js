4f7459cc836a1e2411ea39bd7db6e84bbc01dc40
Cael Formanek <cformanek@chapman.edu>
2024-02-24 13:52:04 -0800

    added link between AuthFlow button and root page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import RootInfoPage from './RootInfoPage';
 import ChooseVerification from './ChooseVerification';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <ChooseVerification></ChooseVerification>
-      {/* <RootInfoPage></RootInfoPage> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ChooseVerification />} />
+        <Route path="/rootinfo" element={<RootInfoPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/ChooseVerification.js
+++ frontend/src/ChooseVerification.js
@@ -12,7 +12,7 @@ function ChooseVerification() {
         <div className='methodbuttons'>
           <div><button className='sendtoemail'>Send Code to Email</button></div>
           <div><button className='receivesms'>Receive SMS Code</button></div>
-          <div><button className='useauthflow'>Use AuthFlow Application</button></div>
+          <div><a href='http://localhost:3000/rootinfo'><button className='useauthflow'>Use AuthFlow Application</button></a></div>
         </div>
       </div>
     </div>
